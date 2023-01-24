ce4f2fe1a201649efbda3e7314ba2d3fb9c3bbd0
micsiw <michalsiwanowicz@gmail.com>
2023-01-24 02:20:50 +0100

    players now switch turns and chosing field affect internal array

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const GameBoard = (() => {
             occupied: null
         }]
 
-        const display = (() => {
+        function display() {
             boardPositions.forEach(field => {
                 const boardField = document.createElement('div');
                 boardField.dataset.position = field.position;
@@ -47,7 +47,9 @@ const GameBoard = (() => {
                 boardField.innerHTML = field.occupied;
                 gameBoardDiv.appendChild(boardField)
             })
-        })();
+        };
+
+        display();
 
         const gameField = document.querySelectorAll('.field');
 
@@ -62,15 +64,46 @@ const Player = (name, mark) => {
     return { name, mark }
 };
 
-const gameController = () => {
+const gameStart = (() => {
+
     const firstPlayer = Player('Player1', 'X');
     const secondPlayer = Player('Player2', 'O');
 
-}
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+    ];
+
+    let actualPlayer = firstPlayer;
+
+    function playerAction(event) {
+        const fieldIndex = GameBoard.boardPositions.findIndex(field => field.position === event.dataset.position);
+
+        if (GameBoard.boardPositions[fieldIndex].occupied === null) {
+            GameBoard.boardPositions[fieldIndex].occupied = actualPlayer.mark;
+            event.innerHTML = actualPlayer.mark;
+        }
+
+        if (GameBoard.boardPositions[fieldIndex].occupied === 'X') {
+            actualPlayer = secondPlayer;
+        } else {
+            actualPlayer = firstPlayer;
+        }
+        console.log(fieldIndex)
+        console.log(GameBoard.boardPositions[fieldIndex].occupied)
+        console.log(event)
+    }
+
+
+
+    return { playerAction }
+
+    
+})();
 
 
 GameBoard.gameField.forEach(field => {
-    field.addEventListener('click', ()  => {
-        field.innerHTML = 'X'
+    field.addEventListener('click', (event)  => {
+        gameStart.playerAction(event.target);
+
     })
 });
