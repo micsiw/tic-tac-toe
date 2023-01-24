7831e547cd0eebe896cb9422e01cf77ed76218ed
micsiw <michalsiwanowicz@gmail.com>
2023-01-24 19:03:49 +0100

    script manipulates dom, user can see announcements on the screen

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const gameBoardDiv = document.querySelector('[data-game-board]');
 const announcerDiv = document.querySelector('[data-game-announcer]');
+const resetButton = document.querySelector('[data-button-reset]');
+
+const Player = (name, mark) => {
+    return { name, mark }
+};
 
 const GameBoard = (() => { 
     const boardPositions = [
@@ -40,57 +45,58 @@ const GameBoard = (() => {
             occupied: null
         }]
 
-        function displayGrid() {
-            boardPositions.forEach(field => {
-                const boardField = document.createElement('div');
-                boardField.dataset.position = field.position;
-                boardField.classList.add('field');
-                boardField.innerHTML = field.occupied;
-                gameBoardDiv.appendChild(boardField)
+    function displayGrid() {
+        boardPositions.forEach(field => {
+            const boardField = document.createElement('div');
+            boardField.dataset.position = field.position;
+            boardField.classList.add('field');
+            boardField.innerHTML = field.occupied;
+            gameBoardDiv.appendChild(boardField)
+        })
+
+        const gameField = document.querySelectorAll('.field');
+
+        gameField.forEach(field => {
+            field.addEventListener('click', (event)  => {
+                if (GameStart.wonPlayer === null & GameStart.turnCounter < 9) {
+                    GameStart.playerAction(event.target);
+                    GameStart.isOver();
+                    GameStart.turnCounter++
+                    GameStart.announcer();
+                }
             })
+        });
+    }
 
-            const gameField = document.querySelectorAll('.field');
-
-            gameField.forEach(field => {
-                field.addEventListener('click', (event)  => {
-                    if (GameStart.wonPlayer === null) {
-                        GameStart.playerAction(event.target);
-                        GameStart.isOver();
-                    }
-                })
-            });
-        }
-
+    function resetGrid() {
+        gameBoardDiv.innerHTML = '';
+        boardPositions.forEach(position => {
+            position.occupied = null;
+        })
         displayGrid();
+        GameStart.wonPlayer = null;
+        GameStart.turnCounter = 0;
+        GameStart.announcer();
+    }
 
-        function resetGrid() {
-            gameBoardDiv.innerHTML = '';
-            boardPositions.forEach(position => {
-                position.occupied = null;
-            })
-            GameStart.wonPlayer = null;
-            displayGrid();
-        }
+    displayGrid();
 
-        return {
-            boardPositions,
-            resetGrid
-        } 
+    return {
+        boardPositions,
+        resetGrid
+    } 
 })();
 
-const Player = (name, mark) => {
-    return { name, mark }
-};
+
 
 const GameStart = (() => {
-
-    const firstPlayer = Player('Player1', 'X');
-    const secondPlayer = Player('Player2', 'O');
-
+    const firstPlayer = Player('Player X', 'X');
+    const secondPlayer = Player('Player O', 'O');
     const winningCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
     ];
 
+    let turnCounter = 0;
     let actualPlayer = firstPlayer;
     let wonPlayer = null;
 
@@ -100,6 +106,7 @@ const GameStart = (() => {
         if (GameBoard.boardPositions[fieldIndex].occupied === null) {
             GameBoard.boardPositions[fieldIndex].occupied = actualPlayer.mark;
             event.innerHTML = actualPlayer.mark;
+            event.style.pointerEvents = 'none';
         }
 
         if (GameBoard.boardPositions[fieldIndex].occupied === 'X') {
@@ -109,7 +116,7 @@ const GameStart = (() => {
         }
     }
 
-    function isOver () {
+    function isOver() {
 
         winningCombinations.forEach(combination => {
 
@@ -123,10 +130,8 @@ const GameStart = (() => {
                 if (checkMarks[1] !== null && checkMarks[1] === checkMarks[2]) {
                     if (checkMarks[0] === 'X') {
                         GameStart.wonPlayer = firstPlayer;
-                        console.log('PLAYER X WON')
                     } else {
                         GameStart.wonPlayer = secondPlayer;
-                        console.log('PLAYER O WON')
                     }
                 }
             }
@@ -134,9 +139,29 @@ const GameStart = (() => {
         })
     }
 
+    function announcer() {
 
+        if (GameStart.turnCounter === GameBoard.boardPositions.length && GameStart.wonPlayer === null) {
+            announcerDiv.innerHTML = "That's a tie!";
+        } else if (GameStart.wonPlayer === firstPlayer) {
+            announcerDiv.innerHTML = "Player X's won!"
+        } else if (GameStart.wonPlayer === secondPlayer) {
+            announcerDiv.innerHTML = "Player O's won!"
+        } else {
+            actualPlayer === firstPlayer && turnCounter < GameBoard.boardPositions.length ? 
+            announcerDiv.innerHTML = "Player X's turn!" : announcerDiv.innerHTML = "Player O's turn!" };
+    }
 
-    return { playerAction, isOver, wonPlayer }
+    return {
+        playerAction,
+        isOver,
+        announcer,
+        wonPlayer,
+        turnCounter
+    }
 
-    
 })();
+
+resetButton.addEventListener('click', () => {
+    GameBoard.resetGrid();
+})
